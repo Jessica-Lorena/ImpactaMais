4efb65c79a74f13eb4627d7e4f9dfe85cb08d81f
Jessica Cristina dos Santos Lorena <jessicacslorena@gmail.com>
2020-09-10 15:14:11 -0300

    Construção das tabelas em classes

--- impactamais/src/main/java/com/zeroseis/impactamais/model/Tema.java
+++ impactamais/src/main/java/com/zeroseis/impactamais/model/Tema.java
@@ -1,63 +1,83 @@
 package com.zeroseis.impactamais.model;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name = "tema")
+@Table(name = "tb_tema")
 public class Tema {
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private long id_tema;
+	private long id;
 	
 	@NotNull
-	@Size(min = 5, max = 255)
-	private String pedido;
+	@Size(min = 1, max = 255)
+	private String descricao;
 	
 	@NotNull
-	@Size(min = 5, max = 255)
-	private String ajudei;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data_tema = new java.sql.Date(System.currentTimeMillis());
 	
 	@NotNull
-	@Size(min = 5, max = 255)
-	private String info;
+	private boolean ativo;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
 
-	public long getId_tema() {
-		return id_tema;
+	public long getId() {
+		return id;
 	}
 
-	public void setId_tema(long id_tema) {
-		this.id_tema = id_tema;
+	public void setId(long id) {
+		this.id = id;
 	}
 
-	public String getPedido() {
-		return pedido;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setPedido(String pedido) {
-		this.pedido = pedido;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
-	public String getAjudei() {
-		return ajudei;
+	public Date getData_tema() {
+		return data_tema;
 	}
 
-	public void setAjudei(String ajudei) {
-		this.ajudei = ajudei;
+	public void setData_tema(Date data_tema) {
+		this.data_tema = data_tema;
 	}
 
-	public String getInfo() {
-		return info;
+	public boolean isAtivo() {
+		return ativo;
 	}
 
-	public void setInfo(String info) {
-		this.info = info;
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 
 }
